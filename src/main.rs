c7f88b6cc33afc7cd7debe3aacbaf931553b3f3a
xiaoqiang zhao <zxq_yx_007@163.com>
2022-01-31 19:57:37 +0800

    add ray struct

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
 use std::io::{self, Write};
 use vec3::Color;
+use vec3::Point3;
+use ray::Ray;
 mod vec3;
 mod color;
+mod ray;
 
 
 fn main() {
     // Image
-    let image_width = 256;
-    let image_height = 256;
+    let aspect_ratio = 16.0 / 9.0;
+    let image_width = 400;
+    let image_height = (image_width as f64 / aspect_ratio) as i32;
+
+
+    // Camera
+    let viewport_height = 2.0;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length: f64 = 1.0;
+
+    let origin = Point3::new();
+    let horizontal = vec3::Vec3 { e: [viewport_width as f64, 0.0, 0.0] };
+    let vertical = vec3::Vec3 { e: [0.0, viewport_height as f64, 0.0] };
+    let lower_left_corner = origin - horizontal / 2f64 - vertical / 2f64 - vec3::Vec3{ e: [0.0, 0.0, focal_length] }; 
 
     // Render
 
@@ -17,10 +32,11 @@ fn main() {
         eprint!("\rScanlines remaining: {}", j);
         io::stderr().flush().unwrap();
         for i in 0..image_width {
-            let r = i as f64 / (image_width - 1) as f64;
-            let g = j as f64 / (image_height - 1) as f64;
-            let b = 0.25;
-            let pixel_color = Color { e: [r, g, b] };
+            let u = i as f64 / (image_width - 1) as f64;
+            let v = j as f64 / (image_height - 1) as f64;
+            let r = Ray::cons(&origin, &(lower_left_corner + horizontal * u + vertical * v - origin));
+
+            let pixel_color = r.ray_color();
             color::write_color(&mut std::io::stdout(), pixel_color).unwrap();
         }
     }
---
+++ src/ray.rs
@@ -0,0 +1,47 @@
+use crate::vec3::Point3;
+use crate::vec3;
+use crate::vec3::Color;
+
+pub struct Ray {
+
+    /// Origin point
+    pub orig: Point3,
+    /// direction
+    pub dir: vec3::Vec3,
+
+}
+
+
+impl Ray {
+
+    pub fn new() -> Self {
+        Ray { orig: Point3::new(), dir: vec3::Vec3::new() }
+    }
+
+    pub fn cons(origin: &Point3, direction: &vec3::Vec3) -> Self {
+
+        Ray { orig: *origin, dir: *direction }
+    }
+
+
+    pub fn origin(&self) -> Point3 {
+        self.orig
+    }
+
+    pub fn direction(&self) -> vec3::Vec3 {
+        self.dir
+    }
+
+    pub fn at(&self, t: f64) -> Point3 {
+        self.orig + self.dir * t
+    }
+
+    pub fn ray_color(&self) -> Color {
+
+        let color = Color { e: [1.0, 1.0, 1.0] };
+        let color2 = Color { e: [0.5, 0.7, 1.0]};
+        let unit_direction = self.direction().unit_vector();
+        let t = 0.5 * (unit_direction.y() + 1.0);
+        color * (1.0 - t) +  color2 * t
+    }
+}
