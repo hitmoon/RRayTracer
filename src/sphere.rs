be924cc5e45a452bd0dfd7c1c39e6dfa2dae0b0a
xiaoqiang zhao <zxq_yx_007@163.com>
2022-02-14 14:11:34 +0800

    simplifying ray sphere intersection code

--- src/sphere.rs
+++ src/sphere.rs
@@ -3,12 +3,12 @@ use crate::ray::Ray;
 
 pub fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
     let oc = r.origin() - *center;
-    let a = r.direction().dot(&r.direction());
-    let b = 2.0 * oc.dot(&r.direction());
-    let c = oc.dot(&oc) - radius * radius;
-    let discriminant = b * b - 4.0 * a * c;
+    let a = r.direction().length_squared();
+    let half_b = oc.dot(&r.direction());
+    let c = oc.length_squared() - radius * radius;
+    let discriminant = half_b * half_b - a * c;
     if discriminant < 0.0 {
         return -1.0;
     }
-    (-b - f64::sqrt(discriminant)) / (2.0 * a)
+    (-half_b - f64::sqrt(discriminant)) / a
 }
