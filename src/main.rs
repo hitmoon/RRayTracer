eb20d4d03f21e2a18ad9543d9907541fed25b011
xiaoqiang zhao <zxq_yx_007@163.com>
2022-02-26 19:38:58 +0800

    the final scene

--- src/main.rs
+++ src/main.rs
@@ -20,41 +20,68 @@ mod util;
 mod camera;
 mod material;
 
+fn random_scene() -> World {
+
+    let mut world = World::new();
+    let ground = Rc::new(Lambertian::new(&Color::from(0.5, 0.5, 0.5)));
+    world.add(Box::new(Sphere::from(&Point3::from(0.0, -1000.0, 0.0), 1000.0, ground)));
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = util::random_double();
+            let center = Point3::from(a as f64 + 0.9 * util::random_double(),
+                                      0.2,
+                                      b as f64 + 0.9 * util::random_double());
+            if (center - Point3::from(4.0, 0.2, 0.0)).length() > 0.9 {
+                if choose_mat < 0.8 {
+                    // diffuse
+                    let albedo = Color::random() * Color::random();
+                    let material = Rc::new(Lambertian::new(&albedo));
+                    world.add(Box::new(Sphere::from(&center, 0.2, material.clone())));
+                } else if choose_mat < 0.95 {
+                    // metal
+                    let albedo = Color::random_range(0.5, 1.0);
+                    let fuzz = util::random_double_range(0.0, 0.5);
+                    let material = Rc::new(Metal::new(&albedo, fuzz));
+                    world.add(Box::new(Sphere::from(&center, 0.2, material.clone())));
+                } else {
+                    // glass
+                    let material = Rc::new(Dielectric::new(1.5));
+                    world.add(Box::new(Sphere::from(&center, 0.2, material.clone())));
+                }
+            }
+        }
+    }
+
+    let mat1 = Rc::new(Dielectric::new(1.5));
+    world.add(Box::new(Sphere::from(&Point3::from(0.0, 1.0, 0.0), 1.0, mat1.clone())));
+
+    let mat2 = Rc::new(Lambertian::new(&Color::from(0.4, 0.2, 0.1)));
+    world.add(Box::new(Sphere::from(&Point3::from(-4.0, 1.0, 0.0), 1.0, mat2.clone())));
+
+    let mat3 = Rc::new(Metal::new(&Color::from(0.7, 0.6, 0.5), 0.0));
+    world.add(Box::new(Sphere::from(&Point3::from(4.0, 1.0, 0.0), 1.0, mat3.clone())));
+
+    world
+}
+
 fn main() {
     // Image
-    let aspect_ratio = 16.0 / 9.0;
-    let image_width = 400;
+    let aspect_ratio = 3.0 / 2.0;
+    let image_width = 1200;
     let image_height = (image_width as f64 / aspect_ratio) as i32;
-    let samples_per_pixel: i32 = 100;
+    let samples_per_pixel: i32 = 500;
     let max_depth = 50;
 
     // World
-    let R = f64::cos(f64::consts::PI / 4.0);
-    let mut world = World::new();
-    /*
-    let left = Rc::new(Lambertian::new(&Color::from(0.0, 0.0, 1.0)));
-    let right = Rc::new(Lambertian::new(&Color::from(1.0, 0.0, 0.0)));
-
-    world.add(Box::new(Sphere::from(&Point3::from(-R, 0.0, -1.0), R, left.clone())));
-    world.add(Box::new(Sphere::from(&Point3::from(R, 0.0, -1.0), R, right.clone())));
-    */
-    let ground = Rc::new(Lambertian::new(&Color::from(0.8, 0.8, 0.0)));
-    let center = Rc::new(Lambertian::new(&Color::from(0.1, 0.2, 0.5)));
-    let left = Rc::new(Dielectric::new(1.5));
-    let right = Rc::new(Metal::new(&Color::from(0.8, 0.6, 0.2), 0.0));
-
-    world.add(Box::new(Sphere::from(&Point3::from(0.0, -100.5, -1.0), 100.0, ground)));
-    world.add(Box::new(Sphere::from(&Point3::from(0.0, 0.0, -1.0), 0.5, center)));
-    world.add(Box::new(Sphere::from(&Point3::from(-1.0, 0.0, -1.0), 0.5, left.clone())));
-    world.add(Box::new(Sphere::from(&Point3::from(-1.0, 0.0, -1.0), -0.45, left.clone())));
-    world.add(Box::new(Sphere::from(&Point3::from(1.0, 0.0, -1.0), 0.5, right)));
+    let world = random_scene();
 
     // Camera
-    let lookfrom = Point3::from(3.0, 3.0, 2.0);
-    let lookat = Point3::from(0.0, 0.0, -1.0);
+    let lookfrom = Point3::from(13.0, 2.0, 3.0);
+    let lookat = Point3::from(0.0, 0.0, 0.0);
     let vup = Vec3::from(0.0, 1.0, 0.0);
-    let dist_to_focus = (lookfrom - lookat).length();
-    let aperture = 2.0;
+    let dist_to_focus = 10.0;
+    let aperture = 0.1;
     let cam = Camera::new(&lookfrom, &lookat, &vup, 20.0, aspect_ratio, aperture, dist_to_focus);
 
     // Render
