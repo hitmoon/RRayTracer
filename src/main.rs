3c50e34d32f762094ee37413b5898702f6216239
xiaoqiang zhao <zxq_yx_007@163.com>
2022-02-26 17:31:31 +0800

    schlick approximation glass

--- src/main.rs
+++ src/main.rs
@@ -36,7 +36,8 @@ fn main() {
 
     world.add(Box::new(Sphere::from(&Point3::from(0.0, -100.5, -1.0), 100.0, ground)));
     world.add(Box::new(Sphere::from(&Point3::from(0.0, 0.0, -1.0), 0.5, center)));
-    world.add(Box::new(Sphere::from(&Point3::from(-1.0, 0.0, -1.0), 0.5, left)));
+    world.add(Box::new(Sphere::from(&Point3::from(-1.0, 0.0, -1.0), 0.5, left.clone())));
+    world.add(Box::new(Sphere::from(&Point3::from(-1.0, 0.0, -1.0), -0.4, left.clone())));
     world.add(Box::new(Sphere::from(&Point3::from(1.0, 0.0, -1.0), 0.5, right)));
 
     // Camera
--- src/material.rs
+++ src/material.rs
@@ -2,6 +2,7 @@ use crate::hittable::HitRecord;
 use crate::ray::Ray;
 use crate::vec3::Color;
 use crate::vec3::Vec3;
+use crate::util;
 
 pub trait Material {
 
@@ -69,7 +70,7 @@ impl Material for Dielectric {
         let sin_theta = f64::sqrt(1.0 - cos_theta * cos_theta);
         let cannot_refract = refraction_ratio * sin_theta > 1.0;
         let mut direction = Vec3::new();
-        if cannot_refract {
+        if cannot_refract || self.reflectance(cos_theta, refraction_ratio) > util::random_double() {
             direction = Vec3::reflect(&unit_direction, &rec.normal);
         } else {
             direction = Vec3::refract(&unit_direction, &rec.normal, refraction_ratio);
@@ -83,5 +84,12 @@ impl Material for Dielectric {
 impl Dielectric {
     pub fn new(index_of_refraction: f64) -> Dielectric {
         Self { ir: index_of_refraction }
+    }
+
+    fn reflectance(&self, cosine: f64, ref_idx: f64) -> f64 {
+        // Use Schlick's approximation for reflectance
+        let mut r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
+        r0 = r0 * r0;
+        r0 + (1.0 - r0) * (1.0 - cosine).powf(5.0)
     }
 }
